448c5c05b5d3c3841ed30b0f918894439487048a
Yohann Bosco <yohann.bosco@gmail.com>
2018-03-14 12:26:21 +0100

    link fetch_posts action to reducer_posts and reducer to post_index component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=yoTest';
+const API_KEY = '?key=123';
 
-export funtion fetchPosts() {
-  const request = axios.get('${ROOT_URL}/posts${API_KEY}');
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
