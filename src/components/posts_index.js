db44b3f1dab7be41902953b57e448a8fe8656f2e
Yohann Bosco <yohann.bosco@gmail.com>
2018-03-14 12:46:28 +0100

    render posts list on screen

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,19 +1,34 @@
+import _ from 'lodash';
 import React,  { Component} from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/index';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
       this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+            {post.title}
+        </li>);
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
-
-export default connect(null, { fetchPosts }) (PostsIndex);
+function mapStatetoProps (state) {
+  return { posts: state.posts };
+}
+export default connect(mapStatetoProps, { fetchPosts }) (PostsIndex);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
-import PostReducer from './reducer_posts';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    posts: PostsReducer
+  
 });
 
 export default rootReducer;
